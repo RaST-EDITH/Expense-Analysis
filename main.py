448008aa9f1cfbb779de07a0f16bc9feb2e12dd7
RaST-EDITH <raghvsinghtomar1975@gmail.com>
2023-09-07 23:44:39 +0530

    Root widget created

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Expense analysis")
+# Library and Modules used 
+from tkinter import *                                        # pip install tkinter==8.6
+import customtkinter as ctk                                  # pip install customtkinter==4.6.3
+
+if __name__ == "__main__" :
+
+    global root
+
+    # Defining Main theme of all widgets
+    ctk.set_appearance_mode( "dark" )
+    ctk.set_default_color_theme( "dark-blue" )
+    wid = 1200
+    hgt = 700
+    root = ctk.CTk()
+    root.title( "Expense Analysis" )
+    root.geometry( "1200x700+200+80" )
+    root.resizable( False, False )
+    root.mainloop()
